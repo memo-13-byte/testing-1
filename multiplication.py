27db96765cbdbce38db3d0207f139d7825a417b8
Mehmet Oğuz <canmehmetoguz@gmail.com>
2020-05-31 14:07:37 +0300

    easy.py-->multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+İlgili dal: master
+Gönderilecek değişiklikler:
+  (izlemeyi kaldırmak için "git reset HEAD <dosya>..." kullanın)
+
+	silindi:      "\303\247arpma.py"
+
--- çarpma.py
+++
@@ -1,2 +0,0 @@
-def çarpma (x,y);
-return x*y;
